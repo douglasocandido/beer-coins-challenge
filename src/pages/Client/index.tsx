a0315ab08fd0a4f42ad9f34f104c82a0dfea55ea
Lucas Lacava <lucas.lacava@ab-inbev.com>
2020-10-24 09:57:56 -0300

    First version of carousel component for cliente screen

--- src/pages/Client/index.tsx
+++ src/pages/Client/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Presentation from '../../components/Presentation/index';
+import CarouselComponent from "../../components/Carousel/index"
 import NavbarComponent from '../../components/NavbarComponent';
-import { 
+import {
   Row,
   Col,
   Button
@@ -16,17 +17,17 @@ import './style.scss'
 const receipts: IReceiptData[] = [
   {
     id: '23145656',
-    value: 200, 
+    value: 200,
     operationDate: new Date()
   },
   {
     id: '2342344',
-    value: 500, 
+    value: 500,
     operationDate: new Date()
   },
   {
     id: '7907890',
-    value: 1000, 
+    value: 1000,
     operationDate: new Date()
   }
 ];
@@ -35,28 +36,28 @@ const operations: IOperationData[] = [
   {
     id: '654651',
     beneficiary: 'Sheldon Cooper',
-    value: 200, 
+    value: 200,
     operationDate: new Date(),
     operationType: 'Transferência'
   },
   {
     id: '23234',
     beneficiary: 'Rajesh Koothrappali',
-    value: 200, 
+    value: 200,
     operationDate: new Date(),
     operationType: 'Transferência'
   },
   {
     id: '53563563',
     beneficiary: 'Leonard Hofstadter',
-    value: 200, 
+    value: 200,
     operationDate: new Date(),
     operationType: 'Transferência'
   },
   {
     id: '2146980',
     beneficiary: 'Howard Wolowitz',
-    value: 200, 
+    value: 200,
     operationDate: new Date(),
     operationType: 'Transferência'
   }
@@ -71,17 +72,19 @@ export default function Client() {
         <Col xs={6}>
           <Row className="justify-content-center" style={{ marginBottom: '20px' }}>
             <h2>Últimos lançamentos</h2>
-            <ReceiptTable receiptData={receipts}/>
+            <ReceiptTable receiptData={receipts} />
             <Button className='regular-outline-button' variant="outline-warning" href="/receipt">Ver todos os lançamentos</Button>
           </Row>
           <Row className="justify-content-center" style={{ marginBottom: '20px' }}>
             <h2>Extrato</h2>
-            <OperationsTable operationData={operations}/>
+            <OperationsTable operationData={operations} />
             <Button className='regular-outline-button' variant="outline-warning" href="/operations">Ver extrato completo</Button>
           </Row>
         </Col>
         <Col className='rewards-preview-container'>
-          <div className='rewards-preview'></div>
+          <div className='rewards-preview'>
+            <CarouselComponent />
+          </div>
         </Col>
       </Row>
     </>
