2fa0e88d79e51fd3a0adf0890a087e63c35b03d0
Renato Pacheco Vieira <rvieira@daitan.com>
2020-10-24 16:26:18 -0400

    fix: linting errors

--- src/pages/Client/Client.tsx
+++ src/pages/Client/Client.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Presentation from '../../components/Presentation';
 import CarouselComponent from "../../components/Carousel/index"
 import NavbarComponent from '../../components/NavbarComponent';
